6ea1f19e0bbc3a5577fdc14851e3cb93eec41a78
Luis Ascencio <luiscarlosasqu@ufps.edu.co>
2022-12-09 00:03:09 -0500

    Xd

--- src/controller/FutbolController.js
+++ src/controller/FutbolController.js
@@ -6,7 +6,14 @@ const createPlayer = async (req, res) =>{
     const {nombre, equipo, posicion, dorsal} = req.body;
 
     try{
-
+        const [rows] = await pool.query("INSERT INTO jugadores (nombre, equipo, posicion, dorsal) VALUES (?,?,?)",[nombre, equipo, posicion, dorsal])
+        res.send({
+            id: rows.insertId,
+            nombre,
+            equipo,
+            posicion,
+            dorsal
+        });
     }catch(error){
         return res.status(500).json(
         {
